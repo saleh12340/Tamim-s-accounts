17349257558676052e4840109978640223f8e331
saleh12340 <salthbiany@gmail.com>
2026-09-13 22:26:21 +0300

    fix JVM target compatibility for Android build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,25 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
-android { namespace = "com.saleh.tamimaccounts"; compileSdk = 35
-    defaultConfig { applicationId = "com.saleh.tamimaccounts"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.saleh.tamimaccounts"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.saleh.tamimaccounts"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
